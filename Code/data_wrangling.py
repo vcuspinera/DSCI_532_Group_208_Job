30334be8b3ba1cf59e55d112f4d0d3083a590a09
luhuayue <luhuayueapp@163.com>
2019-12-06 10:53:52 -0800

    sperated app.py and make improvement on plot

--- Code/data_wrangling.py
+++ Code/data_wrangling.py
@@ -8,14 +8,14 @@ mini_sd = data.groupby(['job']).std().sort_values(by=['perc'],
         
 mini_sd = mini_sd.rename(columns = {'perc':'std'})
 all1 = pd.merge(data, mini_sd, how="inner", on="job")
-first = all1.query('sex == "men"')
-second = all1.query('sex == "women"')
+first = all1.query('sex == "men"') #get male only
+second = all1.query('sex == "women"') #get gemale only
 first = first.reset_index()
-first['together'] = pd.Series(np.asarray(first['perc']) + np.asarray(second['perc']))
-first = first.replace({'together' : 0}, value = np.nan)
-first = first.interpolate()
+first['together'] = pd.Series(np.asarray(first['perc']) + np.asarray(second['perc']))#sum up male and female
+first = first.replace({'together' : 0}, value = np.nan) #zeros are actually missing value
+first = first.interpolate() #use interpolation to predict
 first = first.sort_values(by=['std'], 
-                        ascending=[True])
+                        ascending=[True])#reorder to find top 10
 first = first.reset_index()
 
 first2 = data.query('sex == "men"')
@@ -32,6 +32,6 @@ first2['job'] = first2['job'].str.replace('Professor.*', 'Professor', regex=True
 first3 = first2[first2['year'].isin(['2000'])]
 first3 = first3.sort_values(by=['perc'], ascending=[False]).iloc[:10]
 
-all2 = pd.merge(first2, first3, how="inner", on="job")
+all2 = pd.merge(first2, first3, how="inner", on="job")#same logic to get top 10 popular
 all2.to_csv('2000_pop_job.csv')
 all1.to_csv("most_stable_job.csv")
--- Code/make_plot.py
+++ Code/make_plot.py
@@ -5,8 +5,32 @@ import pandas as pd
 from Code.data_wrangling import *
 
 def make_plot(x1 = 'job',y1 = 'std',x2 = 'year_x:O',y2 = 'together', z = 'both'):
-    # Don't forget to include imports
+    '''
+    Make corresponding plots based on the input. Default is make both 
+    (bar + line). If 'bar' is input then make bar plot only.
+
+
+    Parameters
+    ----------
+    x1: str
+        The string that is x axis for bar.
+    y1: str
+        The string that is y axis for line.
+    x2: str
+        The string that is x axis for bar for popularity.
+    y2: str
+        The string that is y axis for linefor popularity.
+    z: str
+        The string to specify what plot to make
+    Returns
+    -------
+    altair plot 
+        A wantted plot.   
+    '''
     def mds_special():
+        '''
+        This is MDS special theme for altair plot
+        '''
         font = "Arial"
         axisColor = "#000000"
         gridColor = "#DEDDDD"
@@ -70,7 +94,7 @@ def make_plot(x1 = 'job',y1 = 'std',x2 = 'year_x:O',y2 = 'together', z = 'both')
     # Create a plot from the cars dataset
    
     if y1 =='std':
-        brush = alt.selection_interval(
+        brush = alt.selection_multi(
         encodings=['x'] # limit selection to x-axis (year) values
         )
 
@@ -78,18 +102,22 @@ def make_plot(x1 = 'job',y1 = 'std',x2 = 'year_x:O',y2 = 'together', z = 'both')
         bar = alt.Chart(first).mark_bar().add_selection(
             brush
         ).encode(
-            alt.X(x1, title='Job', axis=alt.Axis(labelAngle=30)),
-            alt.Y(y1, title= 'Standard Diveation')
+            alt.X(x1, title='Job', axis=alt.Axis(labelAngle=30),
+            sort=alt.EncodingSortField(
+            field=y1,  # The field to use for the sort
+            order="ascending")),
+            alt.Y(y1, title= 'Standard Diveation'),
+            color=alt.condition(brush, 'job', alt.value('grey'))
         ).properties(
             width=400,
             height=400,
             title = 'SD of Ten Most Stable Jobs from 1850 to 2000'
         )
 
-        # scatter plot, modify opacity based on selection
+        # line plot, modify opacity based on selection
         line = alt.Chart(first).mark_line().encode(
             alt.X(x2, title = 'Year', axis=alt.Axis(labelAngle=0)),
-            alt.Y(y2, title = 'Percentage in Total Work Force'),
+            alt.Y(y2, title = 'Percentage in Total Work Force', axis=alt.Axis(format='%')),
             color = 'job',
             opacity=alt.condition(brush, alt.value(0.75), alt.value(0.05))
         ).properties(
@@ -99,7 +127,7 @@ def make_plot(x1 = 'job',y1 = 'std',x2 = 'year_x:O',y2 = 'together', z = 'both')
         )
 
     if y1 == 'together_y':
-        brush = alt.selection_interval(
+        brush = alt.selection_multi(
         encodings=['x'] # limit selection to x-axis (year) values
         )
 
@@ -107,18 +135,22 @@ def make_plot(x1 = 'job',y1 = 'std',x2 = 'year_x:O',y2 = 'together', z = 'both')
         bar = alt.Chart(all2).mark_bar().add_selection(
             brush
         ).encode(
-            alt.X(x1, title='Job', axis=alt.Axis(labelAngle=30)),
-            alt.Y(y1, title= 'Percentage')
+            alt.X(x1, title='Job', axis=alt.Axis(labelAngle=30),
+            sort=alt.EncodingSortField(
+            field=y1,  # The field to use for the sort
+            order="descending")),
+            alt.Y(y1, title= 'Percentage',axis=alt.Axis(format='%')),
+            color=alt.condition(brush, 'job', alt.value('grey'))
         ).properties(
             width=400,
             height=400,
             title = 'Ten Most Popular Jobs in Year 2000'
         )
 
-        # scatter plot, modify opacity based on selection
+        # scatter line, modify opacity based on selection
         line = alt.Chart(all2).mark_line().encode(
             alt.X('year_x:O', title = 'Year', axis=alt.Axis(labelAngle=0)),
-            alt.Y('together_x', title = 'Percentage in Total Work Force'),
+            alt.Y('together_x', title = 'Percentage in Total Work Force',axis=alt.Axis(format='%')),
             color = 'job',
             opacity=alt.condition(brush, alt.value(0.75), alt.value(0.05))
         ).properties(
